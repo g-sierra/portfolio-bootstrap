3a3f999386d1a26a619300e5ae577e192a29fe25
g-sierra <193622299+g-sierra@users.noreply.github.com>
2025-08-29 20:58:21 -0300

    Commit inicial

---
+++ js/main.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // hover en los iconos del footer
+    const links = document.querySelectorAll("#icons a");
+
+    links.forEach(link => {
+        link.addEventListener("mouseover", () => {
+            link.classList.remove("text-white");
+            link.classList.add("text-secondary");
+        });
+        link.addEventListener("mouseout", () => {
+            link.classList.remove("text-secondary");
+            link.classList.add("text-white");
+        });
+    });
+});
